541598b67eecf739b28383309a82d2002ad47206
FAN3XD <57396519+FAN3XD@users.noreply.github.com>
2020-08-15 21:00:53 +0800

    用IDEA写的java程序
    用IDEA写的java程序

---
+++ IDEA-java/math01/src/Math01/Math01.java
@@ -0,0 +1,18 @@
+package Math01;
+
+import java.util.Scanner;
+
+public class Math01 {
+
+    public static void main(String[] args) {
+        for (int i=1; i<=9; i++)
+        {
+            for (int j=1; j<=i; j++)
+            {
+                System.out.print(j + "x" + i + "=" + (i*j) + "\t");
+            }
+            System.out.println();
+        }
+
+    }
+}
